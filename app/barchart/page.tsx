6ecee50b5887beb81ec66c1d9a5e28d361ea369c
ishi720 <ishizuka720@gmail.com>
2025-02-09 00:20:58 +0900

    並び替えとアニメーションの実装

--- app/barchart/page.tsx
+++ app/barchart/page.tsx
@@ -1,10 +1,16 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Vizzu from 'vizzu';
 
-const Barchart = () => {
+const Barchert = () => {
+
+  // 初回表示
+  const [chart, setChart] = useState(null);
+  const [sorted, setSorted] = useState(false);
+
   useEffect(() => {
-    const chart = new Vizzu('chart');
+    const vizzuChart = new Vizzu('chart');
+    setChart(vizzuChart);
 
     const data = {
       series: [
@@ -13,7 +19,7 @@ const Barchart = () => {
       ]
     };
 
-    chart.animate({
+    vizzuChart.animate({
       data: data,
       config: {
         channels: {
@@ -24,11 +30,47 @@ const Barchart = () => {
     });
   }, []);
 
+
+  // 並び替え
+  const handleSort = () => {
+    if (!chart) return;
+
+    const originalData = [
+      { x: 'A', y: 5 },
+      { x: 'B', y: 10 },
+      { x: 'C', y: 7 }
+    ];
+
+    const sortedData = [...originalData].sort((a, b) => sorted ? a.y - b.y : b.y - a.y);
+    setSorted(!sorted);
+
+    chart.animate({
+      data: {
+        series: [
+          { name: 'x', values: sortedData.map(item => item.x) },
+          { name: 'y', values: sortedData.map(item => item.y) }
+        ]
+      },
+      config: {
+        channels: {
+          x: { set: 'x' },
+          y: { set: 'y' }
+        }
+      }
+    });
+  };
+
+  //画面描画
   return (
     <div>
+      <button 
+        onClick={handleSort}
+        className={`px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded `}>
+        並び替え
+      </button>
       <div id="chart" style={{ width: '600px', height: '400px' }}></div>
     </div>
   );
 };
 
-export default Barchart;
+export default Barchert;
