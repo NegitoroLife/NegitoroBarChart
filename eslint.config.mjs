cc541c41ee51c4ccb45db250c76c54402d64e62b
naitoyuma7110 <naitoyuma1118@gmail.com>
2025-02-09 12:25:03 +0900

    tsconfig修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
 			"@typescript-eslint/no-unused-vars": "warn",
 			"@typescript-eslint/no-explicit-any": "warn",
 			"@typescript-eslint/explicit-module-boundary-types": "warn",
+			"react-hooks/exhaustive-deps": "warn",
 		},
 	},
 ];
