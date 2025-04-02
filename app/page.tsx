a58a57b20ac3cfe36708da04737051ffa205090d
naitoyuma7110 <naitoyuma1118@gmail.com>
2025-04-03 00:28:20 +0900

    supabase接続、chartテーブルの全件取得のサンプルAPI設置

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
+import { apiFetch } from "@/app/lib/fetcher";
 
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { FaArrowDown } from "react-icons/fa";
+export default async function Home() {
+	const data = await apiFetch("/api/supabase/chart");
 
-export default function Home() {
 	return (
 		<div className="flex flex-col items-center min-h-screen p-8 bg-gray-100 dark:bg-gray-900">
 			<header className="mb-8 text-center space-y-1">
@@ -16,24 +14,17 @@ export default function Home() {
 				</p>
 			</header>
 			<main className="flex flex-col items-center">
-				<motion.div
-					className="mt-2"
-					animate={{ y: [0, -10, 0], opacity: [1, 0.5, 1] }}
-					transition={{ repeat: Infinity, duration: 1 }}>
-					<p className="mt-4 text-red-600 dark:text-gray-400">
-						クリックして開始
-					</p>
-					<FaArrowDown
-						className="text-red-600 dark:text-gray-400 mx-auto"
-						size={24}
-					/>
-				</motion.div>
-				<div className="relative mt-2">
-					<Link
-						href={"/chart_list"}
-						className="bg-blue-600 text-white rounded-md border p-2">
-						グラフを作成！
-					</Link>
+				<div className="p-4 flex flex-col gap-y-2 border rounded-md bg-white">
+					<p className="">テストアカウント</p>
+					<p>メールアドレス： negitorolife2025@gmail.com</p>
+					<p>パスワード： negitoroNegitoro-2025</p>
+				</div>
+				<div>
+					{data ? (
+						<pre>{JSON.stringify(data, null, 2)}</pre>
+					) : (
+						<p>データがありません</p>
+					)}
 				</div>
 			</main>
 		</div>
